ba6ae6633a41850ca26ea796d185337a47bc44b8
Jetmir Ahmati <j.ahmati@reply.de>
2022-05-27 10:25:17 +0200

    fix website production path

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const apiUrl = dev ? 'http://localhost:3000' : 'https://62908061df110a705d9ed61d--endearing-churros-c685d8.netlify.app';
+export const apiUrl = dev ? 'http://localhost:3000' : 'https://endearing-churros-c685d8.netlify.app';
 
 // export const apiUrl = 'http://localhost:3000';
+
+console.log("env mode",process.env.NODE_ENV)
