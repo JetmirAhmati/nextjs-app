feadd61c06c396251a09e1dc4ca44392cf5b145c
Jetmir Ahmati <j.ahmati@reply.de>
2022-05-27 12:10:33 +0200

    add getStaticProps for articles also

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -23,7 +23,7 @@ const article = (article) => {
     )
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
     const res = await fetch(`${apiUrl}/api/articles/${context.params.id}`), //apiUrl
         article = await res.json();
 
@@ -35,18 +35,18 @@ export const getServerSideProps = async (context) => {
 }
 
 
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`${apiUrl}/api/articles`)
-//     // const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
-//     const articles = await res.json();
-//     const ids = articles.map(article => article.id)
+export const getStaticPaths = async () => {
+    const res = await fetch(`${apiUrl}/api/articles`)
+    // const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+    const articles = await res.json();
+    const ids = articles.map(article => article.id)
 
-//     const paths = ids.map(id => ({ params: { id: id.toString() } }))
+    const paths = ids.map(id => ({ params: { id: id.toString() } }))
 
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
+    return {
+        paths,
+        fallback: false
+    }
+}
 
 export default article
