eca54162232bd0d14edaa0f6c05bb04b4c71a195
Jetmir Ahmati <j.ahmati@reply.de>
2022-05-27 11:58:25 +0200

    load random images

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const articles = [
         title: 'GitHub introduces dark mode and auto-merge pull request',
         excerpt:
             'GitHub today announced a bunch of new features at its virtual GitHub...',
-        imgSrc: "https://picsum.photos/350/200",
+        imgSrc: "https://picsum.photos/350/200?random=1",
         body:
             'GitHub today announced a bunch of new features at its virtual GitHub Universe conference including dark mode, auto-merge pull requests, and Enterprise Server 3.0. In the past couple of years, almost all major apps have rolled out a dark theme for its users, so why not GitHub?',
     },
@@ -12,7 +12,7 @@ export const articles = [
         id: '2',
         title: 'What’s multi-cloud? And why should developers care?',
         excerpt: 'Most developers don’t care about multi-cloud. But they should...',
-        imgSrc: "https://picsum.photos/350/200",
+        imgSrc: "https://picsum.photos/350/200?random=2",
         body:
             'Most developers don’t care about multi-cloud. But they should. Whether developers know it or not, their companies likely already have a multi-cloud environment.    Multi-cloud is a strategy where a business selects different services from different cloud providers',
     },
@@ -21,13 +21,13 @@ export const articles = [
         title: 'Here is how to make your website more accessible',
         excerpt:
             'An accessible website is one that’s optimized for all people, including those with disabilities...',
-            imgSrc: "https://picsum.photos/350/200",
+            imgSrc: "https://picsum.photos/350/200?random=3",
         body:
             'There are many things to consider when setting up a website, and accessibility is one factor that can sometimes be overlooked. An accessible website is one that’s optimized for all people, including those with impaired vision or hearing, motor difficulties, or learning disabilities',
     },
     {
         id: '4',
-        imgSrc: "https://picsum.photos/350/200",
+        imgSrc: "https://picsum.photos/350/200?random=4",
         title: 'Why open ecosystems are the future of app development',
         excerpt:
             'When app stores entered the mainstream tech culture, they exposed developers to an audience of millions...',
