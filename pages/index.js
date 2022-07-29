ea5001bcc4b2fc8187a582823eabbc926b69e33c
DESKTOP-HH0UCCI\Jetmir <j.ahmati@reply.de>
2022-07-29 12:32:39 +0200

    check browser

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,41 @@ import Head from 'next/head'
 import Article from '../components/ArticleList';
 
 export default function Home({ articles }) {
-  
-  {alert(window.navigator.userAgent)}
+ 
+
+  const checkBroswer = ()=> {
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(userAgent.match(/chrome|chromium|crios/i)){
+        browserName = "chrome";
+      }
+
+      else if(userAgent.match(/firefox|fxios/i)){
+        browserName = "firefox";
+      } 
+
+      else if(userAgent.match(/safari line/i)){
+        browserName = "Line";
+      }
+
+       else if(userAgent.match(/safari/i)){
+        browserName = "safari";
+      }
+
+      else if(userAgent.match(/opr\//i)){
+        browserName = "opera";
+      }
+       else if(userAgent.match(/edg/i)){
+        browserName = "edge";
+      }
+
+      else {
+        browserName="No browser detection";
+      }
+
+      {alert(window.navigator.userAgent, browserName)}
+  }
 
   return (
     <div className='home-page'>
