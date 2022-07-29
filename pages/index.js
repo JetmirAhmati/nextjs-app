d2af0f5773755c0e9b3e268bd7c8cc527b808f33
DESKTOP-HH0UCCI\Jetmir <j.ahmati@reply.de>
2022-07-29 12:50:57 +0200

    new change

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home({ articles }) {
  
 
   const checkBroswer = ()=> {
-    let userAgent = navigator.userAgent;
     let browserName;
+    let userAgent = navigator.userAgent;
     
     if(userAgent.match(/chrome|chromium|crios/i)){
         browserName = "chrome";
@@ -28,6 +28,7 @@ export default function Home({ articles }) {
       else if(userAgent.match(/opr\//i)){
         browserName = "opera";
       }
+
        else if(userAgent.match(/edg/i)){
         browserName = "edge";
       }
@@ -35,9 +36,14 @@ export default function Home({ articles }) {
       else {
         browserName="No browser detection";
       }
+        console.log(browserName)
+    
+        const test =  `${window.navigator.userAgent,"  ",browserName}`;
 
-      {alert(window.navigator.userAgent, browserName)}
+      alert(test)
   }
+
+  checkBroswer();
 
   return (
     <div className='home-page'>
